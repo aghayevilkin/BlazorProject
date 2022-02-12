0334579f84dee7c9addae228a69b1e91c6ea9fd7
ilkin <66771388+aghayevilkin@users.noreply.github.com>
2022-02-12 13:47:58 +0400

    Blazor Client (WASM)

---
+++ HiddenVilla/HiddenVilla_Client/wwwroot/js/common.js
@@ -0,0 +1,14 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Succesful')
+    }
+    if (type === "error") {
+        toastr.error(message, 'Failed')
+    }
+    if (type === "info") {
+        toastr.info(message, 'Info')
+    }
+    if (type === "warning") {
+        toastr.warning(message, 'Warning')
+    }
+}
